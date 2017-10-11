62403d228d7ef459f61a2f91a1b1612fcebd8fc7
Sunit Govind <sunit4155@yahoo.com>
2017-10-11 15:57:34 -0600

    Revert "enable testnet5 support"

--- blockchain/util.py
+++ blockchain/util.py
@@ -17,8 +17,7 @@ else:
     from urllib import urlencode
 
 
-def call_api(resource, data=None, base_url=None):
-    base_url = BASE_URL if base_url is None else base_url
+def call_api(resource, data=None, base_url=BASE_URL):
     try:
         payload = None if data is None else urlencode(data)
         if py_version >= 3 and payload is not None:
